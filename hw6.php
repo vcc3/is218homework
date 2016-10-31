f9eb640b8d06b26c61599755a5f9e2fc75b2075d
vcc3 <vcc3@njit.edu>
2016-10-31 14:29:19 -0400

    added first function

---
+++ hw6.php
@@ -0,0 +1,21 @@
+<?php
+// first function 
+function foo() {
+ 
+    // returns an array of all passed arguments
+    $args = func_get_args();
+ // foreach loop that sets args as k and set to v
+    foreach ($args as $k => $v) {
+        echo "arg".($k+1).": $v\n";
+    } 
+} 
+echo " First function of Number of Args ";
+foo(); 
+foo('hello'); 
+foo('hello', 'world', 'again');
+
+echo '<hr>';
+
+
+
+?>
