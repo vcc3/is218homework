504a0c49f183eb4dc41c1935959d419ad998f119
vcc3 <vcc3@njit.edu>
2016-10-31 14:38:43 -0400

    added memory usagges function

--- hw6.php
+++ hw6.php
@@ -15,13 +15,24 @@ foo('hello');
 foo('hello', 'world', 'again');
 
 echo '<hr>';
+// second function
 $files = glob('*.{php,csv}', GLOB_BRACE);
 $files =array_map('realpath',$files);
 echo " Second function of Global and full patchs ";
 print_r($files);
 
 echo '<hr>';
-
+// third function
+echo " infomration for memory usage";
+echo "Initial: ".memory_get_usage()." in bytes \n";
+for ($i = 0; $i < 100000; $i++) {
+    $array []= md5($i);
+}
+for ($i = 0; $i < 100000; $i++) {
+    unset($array[$i]);
+}
+echo "Final: ".memory_get_usage()." bytes \n";
+echo "Peak: ".memory_get_peak_usage()." bytes \n";
 
 
 
