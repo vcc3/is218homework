50b29ea4db6945391284d0437a67f287ec6b9355
vcc3 <vcc3@njit.edu>
2016-11-15 13:12:52 -0500

    added wrong file before

--- globals/server.php
+++ globals/server.php
@@ -1,1 +1,24 @@
- yes
+<?php
+//victor carlos cornejo
+// php code for $_Server. get information from server to output and read. not all information is given back from server.
+
+// echo out the AFS server names
+echo " This server is named : ". $_SERVER['SERVER_NAME'];
+echo '<br>';
+// output the server address
+echo " This servers address is : ". $_SERVER['SERVER_ADDR'];
+echo '<br>';
+//output the software we are using
+echo " This server is using the : ". $_SERVER['SERVER_SOFTWARE'];
+echo '<br>';
+// what method i used to get the page
+echo " The method used to call was : ". $_SERVER['REQUEST_METHOD'];
+echo '<br>';
+// time it took for request
+echo " The time it took for request is  : ". $_SERVER['REQUEST_TIME'];
+echo '<br>';
+// rmomte ports 
+echo " The port used is : ". $_SERVER['REMOTE_PORT'];
+echo '<br>';
+
+?>
