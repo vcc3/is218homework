21ca5bb5f482e17ba9d8c9ebe09cb04ad3bb2b90
vcc3 <vcc3@njit.edu>
2016-11-15 13:00:17 -0500

    added working code and comments to global

--- globals/global.php
+++ globals/global.php
@@ -1,1 +1,14 @@
-yes
+<?php
+function testing() {
+    $information = " Example text in here.";
+    //will be using the glpbal set below.
+    echo 'information in global is: ' . $GLOBALS["information"];
+    echo '<br>';
+    // This echos only the text that was set in $information at the top. this is different from the global declared below.
+    echo 'information in just regular is still: ' . $information;
+}
+// this sets the $information with text that will be set in the $Global echo.
+$information = "Global Portion";
+testing();
+
+?>
