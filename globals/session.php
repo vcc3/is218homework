11725986918be3b38f8553fb810ce46f745832f2
vcc3 <vcc3@njit.edu>
2016-11-29 12:49:19 -0500

    added example  code and comments for why it wont output on screen

--- globals/session.php
+++ globals/session.php
@@ -1,2 +1,10 @@
-yes
-
+<?php
+//starts a session
+session_start();
+//set session to $value
+$_SESSION["newsession"]=$value;
+//echo out  the session
+echo $_SESSION["newsession"];
+//nothing will appear because there is no real seession to record. this is an exmaple of $_session
+echo ' please look at commments in code for why nothing is displayed. this is o  eof the globals that has nothing to display because a lack of an actual session happening';
+?>
