d5ddbab90b42d3ef15298142bd43570b5f958901
vcc3 <vcc3@njit.edu>
2016-11-29 12:53:33 -0500

    added example code  and comments on outputs

--- globals/request.php
+++ globals/request.php
@@ -1,1 +1,17 @@
-yes
+
+<?php
+//storing some values here for calling later.
+$_GET['foo'] = 'a';
+$_POST['bar'] = 'b';
+$_REQUEST['foobar'] ='ab';
+//example of another get,  it will get the 'a'
+var_dump($_GET); 
+echo '<br>';
+//example of another post, posts the stored 'b'
+var_dump($_POST); 
+echo '<br>';
+// example  of the $_request, gets foobar's ab
+var_dump($_REQUEST); 
+echo '<br>';
+
+?>
