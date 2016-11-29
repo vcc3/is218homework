72a1c1fa8230510750844a5d978dad679379597e
vcc3 <vcc3@njit.edu>
2016-11-29 12:57:33 -0500

    added exmaple code and comments

--- globals/env.php
+++ globals/env.php
@@ -1,1 +1,4 @@
-yes
+<?php
+// if there wa s session launched by someone like me Victor,, then it wouldve gotten the USER of the session being my name or whatever i had placed for my USER section. no session i scurrently launched so nothing will output but the typed text.
+echo 'My username is :' .$_ENV["USER"] . ':placeholder is nothing is taken:';
+?>
