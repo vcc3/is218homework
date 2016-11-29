30a29e1b06d3559c3d5ad93d2cbdc7fbc56373e1
vcc3 <vcc3@njit.edu>
2016-11-29 12:44:56 -0500

    added exmaple code and comments

--- globals/cookie.php
+++ globals/cookie.php
@@ -1,1 +1,6 @@
-yes
+
+<?php
+// there are npo cookies for the page so there is nothing to pull for name
+//this is an example but it wont work because of what  afs sends and the lack of coookies for this page.
+echo 'Hello ' . htmlspecialchars($_COOKIE["name"]) . '!';
+?>
